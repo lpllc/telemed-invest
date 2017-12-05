6be7194e121283775c80e1e8f4d9f8423cdcd80e
alikoganyan <alik.oganyan@yandex.ru>
2017-12-05 13:03:40 +0400

    google scripts,new texts

--- frontend/views/layouts/main_ru.php
+++ frontend/views/layouts/main_ru.php
@@ -32,10 +32,22 @@ AppAsset::register($this);
     <?= Html::csrfMetaTags() ?>
     <title>Doctor Smart - инновационная экосистема для цифровой медицины</title>
     <script src='https://www.google.com/recaptcha/api.js'></script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TDJ44T8');</script>
+    <!-- End Google Tag Manager -->
+
     <?php $this->head() ?>
 </head>
 <body>
 <?php $this->beginBody() ?>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TDJ44T8"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
     <?= $content ?>
 
